601f91caa709c4836ab1caeee8d199e693c290c0
briangoss541 <35881754+briangoss541@users.noreply.github.com>
2020-12-17 10:58:25 -0500

    Over-thinking part 1 actually paid dividends

---
+++ 15_2.py
@@ -0,0 +1,49 @@
+data_list = [12, 1, 16, 3, 11, 0]
+# data_list = [0, 3, 6]
+
+memory_dict = dict()
+
+def speak_memory():
+
+    turn_count = 1
+    spoken_number = None
+
+    while turn_count <= 30000000:
+
+        # Starting numbers
+        if turn_count <= len(data_list):
+
+            spoken_number = data_list[turn_count - 1]
+
+            memory_dict[spoken_number] = [turn_count, ]
+
+        else:
+            if spoken_number in memory_dict and len(memory_dict[spoken_number]) >= 2:  # Aged numbers
+
+                age = memory_dict[spoken_number][-1] - memory_dict[spoken_number][-2]
+
+                if age in memory_dict:
+                    memory_dict[age].append(turn_count)
+                else:
+                    memory_dict[age] = [turn_count, ]
+
+                spoken_number = age
+
+            else:  # First-timers
+
+                if 0 in memory_dict:
+                    memory_dict[0].append(turn_count)
+                else:
+                    memory_dict[0] = [turn_count, ]
+
+                spoken_number = 0
+
+        # Aged numbers
+
+        turn_count += 1
+
+    return spoken_number
+
+
+final_num = speak_memory()
+print(final_num)
